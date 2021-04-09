62733d48cc78f88d3f0eff83b93a9e3d3e6542bf
StolenX <trung31891@gmail.com>
2021-04-09 23:14:43 +0900

    use scope, filter in project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
   	@task = current_user.tasks
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,13 +13,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    # @task = current_user.tasks.find(params[:id])
   end
 
   def create
   	# @task = Task.new(task_params.merge(user_id: current_user.id))
     @task = current_user.tasks.new(task_params)
-
+    # @task = current_user.tasks.order(created_at: :desc)
   	if @task.save
   	redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。" 
     else
@@ -28,13 +29,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
+    # task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}』を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
+    # task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク『#{task.name}』を削除しました。 "
   end
@@ -42,6 +43,10 @@ class TasksController < ApplicationController
   private
   def task_params
   	params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 
 end
