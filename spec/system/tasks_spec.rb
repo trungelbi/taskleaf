3932c2e73a3a8a56d64f561d2e6f9cb28480ea35
StolenX <trung31891@gmail.com>
2021-04-12 10:41:23 +0900

    use let for rspec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,20 +1,38 @@
 require 'rails_helper'
 
 describe 'task kanrikikai', type: :system do
+	let!(:user_a) { FactoryBot.create(:user, name: 'usera', email: 'a@gmail.com')}
+	let(:user_b) { FactoryBot.create(:user, name: 'userb', email: 'b@gmail.com')}
+	let!(:task_a) { FactoryBot.create(:task, name: 'first task', user: user_a) }
 
+	before do
+		visit login_path
+		fill_in 'メールアドレス', with: login_user.email
+		fill_in 'パスワード', with: login_user.password
+		click_button 'ログインする'
+	end
 	describe 'hyoujikikai' do
-		before do
-			user_a = FactoryBot.create(:user, name: 'usera', email: 'a@gmail.com')
-			FactoryBot.create(:task, name: 'first task', user: user_a)
-		end
 		context 'dang nhap user a' do
-			before do 
-				visit login_path
-				fill_in 'メールアドレス', with: 'a@gmail.com'
-				fill_in 'パスワード', with: 'password'
-				click_button 'ログインする'
+			let(:login_user) {user_a}
+			it 'task user a dc tao' do
+				expect(page).to have_content 'first task'
 			end
-			it 'task dc tao' do
+		end
+		context 'dang nhap user b' do
+			let(:login_user) {user_b}
+			it 'task user a do not show' do
+				expect(page).to have_no_content 'first task'
+			end
+		end
+	end
+	describe '詳細表示機械' do
+		context 'login user a' do
+			let(:login_user) {user_a}		
+			before do
+				visit task_path(task_a)
+
+			end
+			it 'task user a show' do
 				expect(page).to have_content 'first task'
 			end
 		end
